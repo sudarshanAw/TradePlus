de862726b58ca296da09118712674c1113685e30
sudarAw <sudarshanawasthi47@gmail.com>
2021-02-11 08:55:32 +0100

    first commit

---
+++ TradePlus/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TradePlus project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TradePlus.settings')
+
+application = get_asgi_application()
---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from  django.urls import path,include
+from  rest_framework.authtoken import views
+from .views import home
+
+urlpatterns = [
+    path("", home, name = 'api.home')
+]
